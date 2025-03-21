ed031983c0875f502626d38ce7c7a939018b6621
frederic boileau <frederic.boileau@protonmail.com>
2025-03-21 15:11:32 -0400

    exn caught

--- lib/aur.ml
+++ lib/aur.ml
@@ -130,22 +130,25 @@ exception SubExn of string
 let fetch_exn pkgname = 
     let pkglocation = aur_location ^ "/" ^ pkgname in
     let command = ("git", [|"git";"ls-remote"; "--exit-code"; pkgname|]) in
-    Lwt_process.exec ~stdout:`Dev_null ~stderr:`Dev_null command
-    >>= fun p -> match p with
+    let%lwt p = Lwt_process.exec ~stdout:`Dev_null ~stderr:`Dev_null command in
+    let%lwt () = match p with
         | Unix.WEXITED 0 -> Lwt.return()
-        | Unix.WEXITED _ -> raise (
-            SubExn (Printf.sprintf "Pkg %s is not in AUR\n" pkgname))
+        | Unix.WEXITED _ -> Lwt.fail (SubExn (Printf.sprintf "Pkg %s is not in AUR\n" pkgname))
         | Unix.WSIGNALED _ | Unix.WSTOPPED _ -> raise (Failure "Subprocess error")
-    >>= fun () -> 
+    in
+    let%lwt p = 
         let command = ("git", [|"git"; "clone"; pkglocation|]) in
         Lwt_process.exec ~stdout:`Dev_null ~stderr:`Dev_null command
-    >>= fun p -> match p with
+    in
+    let%lwt () =  match p with
         | Unix.WEXITED 0 -> Lwt.return()
-        | _ -> raise (SubExn (Printf.sprintf "Could not clone %s" pkgname))
+        | _ -> Lwt.fail_with (Printf.sprintf "Could not clone %s" pkgname)
+    in
+    Lwt.return()
 
 
 let fetch pkgname = 
-    try fetch_exn pkgname 
+    try%lwt fetch_exn pkgname 
     with SubExn msg -> Printf.printf "Error: %s" msg; Lwt.return()
 
 (* let exec_exn ~stdout ~stderr command =  *)
--- paste.ml
+++ paste.ml
@@ -1,13 +1,24 @@
-let fetch pkgname = 
-    let command = (
-        "git", 
-        [|"git";"ls-remote"; "--exit-code"; aur_location ^ "/" ^ pkgname|]
-    ) in
-    Lwt_process.exec ~stdout:`Dev_null ~stderr:`Dev_null command
-    >>= (
-        fun p -> match p with
+exception SubExn of string
+let fetch_exn pkgname = 
+    let pkglocation = aur_location ^ "/" ^ pkgname in
+    let command = ("git", [|"git";"ls-remote"; "--exit-code"; pkgname|]) in
+    let%lwt p = Lwt_process.exec ~stdout:`Dev_null ~stderr:`Dev_null command in
+    let%lwt () = match p with
         | Unix.WEXITED 0 -> Lwt.return()
-        | Unix.WEXITED _ -> raise (Failure (Printf.sprintf"Pkg %s is not in AUR\n" pkgname))
+        | Unix.WEXITED _ -> Lwt.fail (SubExn (Printf.sprintf "Pkg %s is not in AUR\n" pkgname))
         | Unix.WSIGNALED _ | Unix.WSTOPPED _ -> raise (Failure "Subprocess error")
-        )
-    >>= fun () -> Lwt_io.printf "Can clone"
+    in
+    let%lwt p = 
+        let command = ("git", [|"git"; "clone"; pkglocation|]) in
+        Lwt_process.exec ~stdout:`Dev_null ~stderr:`Dev_null command
+    in
+    let%lwt () =  match p with
+        | Unix.WEXITED 0 -> Lwt.return()
+        | _ -> Lwt.fail_with (Printf.sprintf "Could not clone %s" pkgname)
+    in
+    Lwt.return()
+
+
+let fetch pkgname = 
+    try fetch_exn pkgname 
+    with SubExn msg -> Printf.printf "Error: %s" msg; Lwt.return()
