cc803716d3c13595b16f53256495d9f885edc0ba
frederic boileau <fred.boileau@gmail.com>
2025-04-22 22:32:02 -0400

    cli refactoring

--- bin/main.ml
+++ bin/main.ml
@@ -1,34 +1,33 @@
-let command = 
-    let open Core in
-    let search = 
-        Command.basic 
-            ~summary:"Search for aur package" 
-            ~readme:(fun () -> "More detailed information")
-            [%map_open.Command
-                let term = anon ("term" %: string)
-                and sort_criteria = anon (maybe ("sort_criteria" %: string)) in
-                    fun () -> Lwt_main.run (Aur.search term)] 
-    in
-    let depends = 
-        Command.basic
-           ~summary:"Get dependencies of pkg"
-           ~readme:(fun () -> "More detailed information")
-           [%map_open.Command
-                let pkgname = anon ("pkgname" %: string) in
-                fun () -> Lwt_main.run (Aur.depends pkgname)]
-    in
-    let fetch = 
-        Command.basic 
-            ~summary:"Fetch aur package"
-            [%map_open.Command
-                let pkgnames  = anon (sequence ("pkgname" %: string)) and
-                syncmode = flag "--syncmode" (optional string)
-                                ~doc:"syncmode when fetching aur repo" and
-                discard = flag "--discard" no_arg
-                                ~doc:"discard local changes when syncing" in
-                fun () -> Lwt_main.run (Aur.fetch pkgnames syncmode discard)]
-    in
-    let chroot =
+open Core
+
+let search = 
+    Command.basic 
+        ~summary:"Search for aur package" 
+        ~readme:(fun () -> "More detailed information")
+        [%map_open.Command
+            let term = anon ("term" %: string)
+            and sort_criteria = anon (maybe ("sort_criteria" %: string)) in
+                fun () -> Lwt_main.run (Aur.search term)] 
+let depends = 
+  Command.basic
+      ~summary:"Get dependencies of pkg"
+      ~readme:(fun () -> "More detailed information")
+      [%map_open.Command
+       let pkgname = anon ("pkgname" %: string) in
+           fun () -> Lwt_main.run (Aur.depends pkgname)]
+
+let fetch = 
+  Command.basic 
+      ~summary:"Fetch aur package"
+      [%map_open.Command
+       let pkgnames  = anon (sequence ("pkgname" %: string)) and
+           syncmode = flag "--syncmode" (optional string)
+                          ~doc:"syncmode when fetching aur repo" and
+           discard = flag "--discard" no_arg
+                         ~doc:"discard local changes when syncing" in
+           fun () -> Lwt_main.run (Aur.fetch pkgnames syncmode discard)]
+
+let chroot =
       Command.basic
         ~summary:"Manage containers for building packages"
         [%map_open.Command
@@ -114,13 +113,11 @@ let command =
            
 
 
-    in
-    Command.group 
+let command = 
+    Command.group
         ~summary:"aur utilities clone of aurutils" 
-        ~readme:(fun () -> "lorem")
-        [("search", search); ("depends", depends);
-         ("fetch", fetch); ("chroot", chroot)]
-                    
+        ~readme:(fun () -> "oaur is the command wrapper for oaurutils.")
+        [("search", search); ("depends", depends); ("fetch", fetch); ("chroot", chroot)]
 
 
 let () = Command_unix.run ~version:"1.0" ~build_info:"RWO" command
